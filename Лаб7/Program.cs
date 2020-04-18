8c5359b609d9cca4f0e54c0c5c04be8d94791f9e
LoliHunter1 <47854086+LoliHunter1@users.noreply.github.com>
2020-04-18 21:53:22 +0300

    Add files via upload

---
+++ Лаб7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Лаб7
+{
+    class Program
+    {
+        class Example
+        {
+            private string fam, status;
+            private int salary, age;
+            public string Fam
+            {
+                set { if (fam == "") fam = value; }
+                get { return (fam); }
+            }
+
+            public int Age
+            {
+                set { age = value; }
+                get { return (age); }
+            }
+
+            public string Status
+            {
+                get { return (status); }
+            }
+
+            public int Salary
+            {
+                set { salary = value; }
+            }   
+
+            private string helth;
+            public Example()
+            {
+                this.fam = "";
+                this.helth = "Не болен короновирусом";
+                this.status = "Холост";
+            }
+            public int get_salary()
+            {
+                return this.salary;
+            }
+
+            public string get_helth()
+            {
+                return this.helth;
+            }
+
+        }
+        static void Main()
+        {
+            Example c = new Example();
+            c.Fam = "Костенко";
+            c.Fam = "hggj";
+            c.Salary = 100000;
+            c.Age = 20;
+            Console.WriteLine("Фамилия = {0}, статус = {1}, зарплата = {2}, возраст = {3}, здоровье = {4}", c.Fam, c.Status, c.get_salary(), c.Age, c.get_helth());
+        }
+    }
+}
